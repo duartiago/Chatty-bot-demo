e51980e771f1a2fba96ade340f1b5145fec092a8
duartiago <74076182+duartiago@users.noreply.github.com>
2020-12-02 14:53:15 +0100

    Add files via upload

---
+++ BlogPost.cs
@@ -0,0 +1,9 @@
+﻿namespace Bloggy.Demo.Domain
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }  
+    }
+}
